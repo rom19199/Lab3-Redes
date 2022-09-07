81f1ddaab747f70f5ee4b385e133754506eefbb6
Laure30 <gom19501@uvg.edu.gt>
2022-09-06 18:15:23 -0600

    CHANGES

--- Distance Vector/DVR.py
+++ Distance Vector/DVR.py
@@ -11,41 +11,41 @@ import asyncio
 # https://www.geeksforgeeks.org/distance-vector-routing-dvr-protocol/
 # https://github.com/ppartarr/pyRoute
 
+class DistanceVectorRouting():
+    def get_ID(docu, JID):
+        file = open(docu, "r")
+        file = file.read()
+        info = eval(file)
+        if(info["type"] =="nombre"):
+            nombre = info["c"]
+            JIDS = {l: k for k, l in nombre.items()}
+            nombre = JIDS[JID]
+            return(nombre)
 
-def get_ID(docu, JID):
-    file = open(docu, "r")
-    file = file.read()
-    info = eval(file)
-    if(info["type"] =="nombre"):
-        nombre = info["c"]
-        JIDS = {l: k for k, l in nombre.items()}
-        nombre = JIDS[JID]
-        return(nombre)
+        else: 
+            raise Exception('ERROR CON EL DOCUMENTO')
 
-    else: 
-        raise Exception('ERROR CON EL DOCUMENTO')
+    def get_JID(docu,ID):
+        file = open(docu, "ID")
+        file = file.read()
+        info = eval(file)
+        if(info["type"]=="nombre"):
+            nombre = info["config"]
+            JID = nombre[ID]
+            return(JID)
+        else:
 
-def get_JID(docu,ID):
-    file = open(docu, "ID")
-    file = file.read()
-    info = eval(file)
-    if(info["type"]=="nombre"):
-        nombre = info["config"]
-        JID = nombre[ID]
-        return(JID)
-    else:
+            raise Exception('ERROR CON EL DOCUMENTO')
 
-        raise Exception('ERROR CON EL DOCUMENTO')
+    def nodo(ejemplo, ID):
+        file = open(ejemplo, "r")
+        file = file.read()
+        info = eval(file)
+        if(info["type"]=="ejemplofile"):
+            nombre = info["config"]
+            nodo_IDs = nombre[ID]
+            return(nodo_IDs)
+        else: 
+            raise Exception ('Error CON EL DOCUMENTO')
 
-def nodo(ejemplo, ID):
-    file = open(ejemplo, "r")
-    file = file.read()
-    info = eval(file)
-    if(info["type"]=="ejemplofile"):
-        nombre = info["config"]
-        nodo_IDs = nombre[ID]
-        return(nodo_IDs)
-    else: 
-        raise Exception ('Error CON EL DOCUMENTO')
-
-    return
+        return
